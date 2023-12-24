aff7208fa63548d7ed8f1119c8e77ee85cc4aeb3
Aylmer Bolzan <aylmer.bolzan@gmail.com>
2023-12-24 07:28:36 -0300

    Aula 10 - Variáveis

---
+++ curso-java-basico/src/com/aylmerbolzan/aula10/Variaveis.java
@@ -0,0 +1,28 @@
+package com.aylmerbolzan.aula10;
+
+public class Variaveis {
+    public static void main(String[] args){
+//        Convenção Java
+        int idade = 20;
+        String nome = "Aylmer";
+        String nomeDoMeuCachorro = "Jimmy";
+
+//        Aceito mas não utilizado
+        int _idade;
+        int $idade;
+        String ano2014 = "2014";
+
+//        Não é convenção Java
+        String nome_do_meu_cachorro;
+        String NomeDoMeuCachorro;
+
+//        Má prática
+        int a = 10;
+        String b = "Bolzan";
+
+        idade = 25;
+
+        System.out.println("Idade = " + idade);
+
+    }
+}
