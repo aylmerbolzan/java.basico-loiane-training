9f8dd4cc9b9f94ec6c47f8e38d816e37bf87a7d9
Aylmer Bolzan <aylmer.bolzan@gmail.com>
2024-01-12 23:59:43 -0300

    Aula 21: ForEach

---
+++ curso-java-basico/src/aula21/ForEach.java
@@ -0,0 +1,58 @@
+package aula21;
+
+import java.util.Random;
+
+public class ForEach {
+    public static void main(String[] args) {
+        
+        int[] notas = new int[10];
+
+        Random random = new Random();
+
+        for (int i = 0; i < notas.length; i++) {
+            notas[i] = random.nextInt(10);
+        }
+
+        for (int i = 0; i < notas.length; i++) {
+            int nota = notas[i];
+            System.out.println(nota);
+        }
+
+        System.out.println("Usando o ForEach:");
+
+        for (int nota : notas) {
+            System.out.println(nota);
+        }
+
+        //Exemplo 2
+        System.out.println("Exemplo com arrays multidimensionais:");
+        double[][] notasAlunos = new double[4][4];
+
+        notasAlunos[0][0] = 10;
+        notasAlunos[0][1] = 7;
+        notasAlunos[0][2] = 9;
+        notasAlunos[0][3] = 9.5;
+
+        notasAlunos[1][0] = 9;
+        notasAlunos[1][1] = 8;
+        notasAlunos[1][2] = 7;
+        notasAlunos[1][3] = 9;
+
+        notasAlunos[2][0] = 5;
+        notasAlunos[2][1] = 6;
+        notasAlunos[2][2] = 8;
+        notasAlunos[2][3] = 7.5;
+
+        notasAlunos[3][0] = 9;
+        notasAlunos[3][1] = 10;
+        notasAlunos[3][2] = 7.8;
+        notasAlunos[3][3] = 7;
+
+        for (double[] notasAluno : notasAlunos) {
+            for (double nota : notasAluno) {
+                System.out.println(nota + " ");
+            }
+            System.out.println();            
+        }
+    }
+}
