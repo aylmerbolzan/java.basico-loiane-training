6c88e28e8cd473707bf2bfe8c6b144095e31de09
Aylmer Bolzan <aylmer.bolzan@gmail.com>
2024-01-18 09:01:17 -0300

    Aula 28: Pacotes (packages) e import

---
+++ curso-java-basico/src/aula28/Pacotes.java
@@ -0,0 +1,13 @@
+package aula28;
+
+import aula26.TesteCarro;
+import aula27.Carro;
+
+public class Pacotes {
+    public static void main(String[] args) {
+        Carro carro;
+        aula26.Carro carro2;
+
+        TesteCarro testeImport;
+    }
+}
