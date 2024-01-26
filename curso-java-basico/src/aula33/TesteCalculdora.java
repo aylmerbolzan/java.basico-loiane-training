31de4479b5065882737bd89ef35d8d6e30a81624
Aylmer Bolzan <aylmer.bolzan@gmail.com>
2024-01-25 22:37:40 -0300

    Aula 33: Sobrecarga de métodos e construtores

---
+++ curso-java-basico/src/aula33/TesteCalculdora.java
@@ -0,0 +1,9 @@
+package aula33;
+
+public class TesteCalculdora {
+    public static void main(String[] args) {
+        MinhaCalculadora calc = new MinhaCalculadora();
+        calc.soma(1, 2);
+        calc.soma(1.0, 2.0);
+    }
+}
