e3da25996cd4c90691345fa82340f50fa1f4abbe
Aylmer Bolzan <aylmer.bolzan@gmail.com>
2024-01-26 08:33:28 -0300

    Aula 34: Variáveis e métodos estáticos (static)

---
+++ curso-java-basico/src/aula34/TesteCalculdora.java
@@ -0,0 +1,12 @@
+package aula34;
+
+public class TesteCalculdora {
+    public static void main(String[] args) {
+        
+    // MinhaCalculadora calc = new MinhaCalculadora();
+    // calc.soma(1,2);
+    
+    MinhaCalculadora.soma(1,2);
+    
+    }
+}
