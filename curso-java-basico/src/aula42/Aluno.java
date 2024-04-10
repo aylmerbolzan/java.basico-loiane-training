49c9802198add2757afc1922c09b1bcc7dfd1e2e
Aylmer Bolzan <aylmer.bolzan@gmail.com>
2024-04-10 00:59:41 -0300

    aula 42: palavra-chave final

---
+++ curso-java-basico/src/aula42/Aluno.java
@@ -0,0 +1,47 @@
+package aula42;
+
+public class Aluno  {
+    
+
+    private String curso;
+    private double[] notas;
+
+    public void verificarAcesso(){
+        // this.nomeVisibilidade = "Maria";
+        // super.nomeVisibilidade = "Teste";
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+    public double[] getNotas() {
+        return notas;
+    }
+    public void setNotas(double[] notas) {
+        this.notas = notas;
+    }
+
+    public double calcularMedia(){
+        return 0;
+    }
+        
+    public boolean verificarArprovado(){
+        return true;
+    }
+
+    public String obterEtiquetaEndereco(){
+        String s = "Endereço do Aluno: ";
+        // s += super.getEndereco();
+
+        return s;
+    }
+
+    public void imprimirEtiquetaEndereco() {
+        System.out.println(this.obterEtiquetaEndereco());
+        throw new UnsupportedOperationException("Unimplemented method 'imprimirEtiquetaEndereco'");
+    }
+
+}
---
+++ curso-java-basico/src/aula42/Constantes.java
@@ -0,0 +1,6 @@
+package aula42;
+
+public class Constantes {
+    public static final String URL_BLOG = "https://aylmer.bolzan.com";
+    public static final String CURSO_COMPLETO = "https://aylmer.bolzan.com/curso";
+}
