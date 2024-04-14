7f0680da4a568b102e8aace0f5ab86ee0b3f7628
Aylmer Bolzan <aylmer.bolzan@gmail.com>
2024-04-14 11:13:44 -0300

    aula 46: interfaces e herança

---
+++ curso-java-basico/src/aula46/BancoMySQL.java
@@ -0,0 +1,71 @@
+package aula46;
+
+public class BancoMySQL implements BancoDados {
+
+    @Override
+    public void grant(String access) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'grant'");
+    }
+
+    @Override
+    public void remoke(String access) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'remoke'");
+    }
+
+    @Override
+    public void select(String query) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'select'");
+    }
+
+    @Override
+    public void insert(String query) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'insert'");
+    }
+
+    @Override
+    public void delete(String query) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+
+    @Override
+    public void update(String query) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void create(String query) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'create'");
+    }
+
+    @Override
+    public void alter(String query) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'alter'");
+    }
+
+    @Override
+    public void drop(String query) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'drop'");
+    }
+
+    @Override
+    public void abrirConexao() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'abrirConexao'");
+    }
+
+    @Override
+    public void fecharConexao() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'fecharConexao'");
+    }
+    
+}
